8f7b12f17eada1f2fefde24517e8349259643ba2
Mara Schulke <mara@schulke.xyz>
2019-10-13 14:54:38 +0200

    feat: add a install script

--- install.sh
+++ install.sh
@@ -1,1 +1,25 @@
-git clone git@github.com:schulke-214/dotfiles.git ~/dotfiles
+#!/bin/sh
+
+DOTFILES=${DOTFILES:-~/dotfiles}
+REPO=${REPO:-schulke-214/dotfiles}
+REMOTE=${REMOTE:-git@github.com:${REPO}.git}
+BRANCH=${BRANCH:-master}
+
+setup_dotfiles() {
+	echo "cloning dotfiles..."
+
+	git clone --depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" || {
+		error "git clone of the $REPO failed"
+		exit 1
+	}
+
+	echo
+}
+
+main() {
+	setup_dotfiles
+
+	echo "source $DOTFILES/zshrc" >> ~/.zshrc
+}
+
+main
