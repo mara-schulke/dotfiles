9547959f7b7c25665b0c29222e6c32d5d606f03b
Mara Schulke <mara@schulke.xyz>
2019-10-13 18:36:08 +0200

    fix: pipe to dev null in the installation script

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ BRANCH=${BRANCH:-master}
 setup_dotfiles() {
 	echo "cloning dotfiles..."
 
-	git clone --depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" &>/dev/null || {
+	git clone --depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" &> /dev/null || {
 		error "git clone of the $REPO failed"
 		exit 1
 	}
 
-	git clone https://github.com/nvm-sh/nvm.git "$DOTFILES/dependencies/nvm" &>/dev/null || {
+	git clone https://github.com/nvm-sh/nvm.git "$DOTFILES/dependencies/nvm" &> /dev/null || {
 		error "git clone of nvm failed"
 		exit 1
 	}
 
-	git clone https://github.com/lukechilds/zsh-nvm "$DOTFILES/zsh/oh-my-zsh/custom/plugins/zsh-nvm" &>/dev/null || {
+	git clone https://github.com/lukechilds/zsh-nvm "$DOTFILES/zsh/oh-my-zsh/custom/plugins/zsh-nvm" &> /dev/null || {
 		error "git clone of zsh-nvm failed"
 		exit 1
 	}
