d17256cf41c4afebde63aa1befc569c43d00c924
Mara Schulke <mara@schulke.xyz>
2019-10-16 08:53:28 +0200

    feat: add a update script

---
+++ update.sh
@@ -0,0 +1,39 @@
+dotfiles-version() {
+	cat "$DOTFILES/version"
+}
+
+dotfiles-latest-version() {
+	curl -fsSL https://raw.githubusercontent.com/schulke-214/dotfiles/master/version
+}
+
+dotfiles-clear-directories() {
+	rm -rf "$DOTFILES/dependencies"
+	rm -rf "$DOTFILES/tmp"
+}
+
+# update functions
+
+main() {
+	echo "updating dotfiles"
+	echo "$(dotfiles-version) -> $(dotfiles-next-version)"
+	echo
+
+	echo "-> clearing dependencies and temp files"
+	dotfiles-clear-directories
+	echo "-> downloading dotfiles $(dotfiles-latest-version)"
+	git -C "$DOTFILES" pull --force
+	echo "-> installing dependencies"
+}
+
+main
+
+#### use arg to specify only deps
+
+# dotfiles-update-deps-only() {
+# 	echo "updating dotfile dependencies"
+# 	echo
+
+# 	echo "-> clearing dependencies and temp files"
+# 	dotfiles-clear-directories
+# 	echo "-> installing dependencies"
+# }
